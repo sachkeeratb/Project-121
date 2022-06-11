34e11b099d492ff4349aafd6495dc65597d9a8ac
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2022-06-10 22:09:34 -0400

    Update blackscreen.py

--- blackscreen.py
+++ blackscreen.py
@@ -1,64 +1,30 @@
 import cv2
-from cv2 import VideoWriter_fourcc
 import numpy as np
 import time
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-vidcap = cv2.VideoCapture('magic.api')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
-output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+video = cv2.VideoCapture(0)
+image = cv2.imread("me.jpeg") 
 
-cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = video.read()
 
-time.sleep(2)
-
-bg = 0
-
-frame = cv2.resize(frame, (640, 480))
-image = cv2.resize(image, (640, 480))
-
-for i in range(60):
-    ret, bg = cap.read()
-
-bg = np.flip(bg, axis = 1)
-
-while(cap.isOpened()):
-    ret, img = cap.read()
-    if(not ret):
-        break
-    
-    img = np.flip(img, axis = 1)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
     
     lower_black = np.array([104, 153, 70])
     upper_black = np.array([30, 30, 0])
 
-    mask = cv2.inRange(hsv, lower_black, upper_black)
+    mask = cv2.inRange(frame, lower_black, upper_black)
 
-    lower_black = np.array([104, 153, 70])
-    upper_black = np.array([30, 30, 0])
-
-
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8()))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3,3), np.uint8()))
-
-
-    res = cv2.bitwise_and(img, img, mask=mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
     f = frame - res
     f = np.where(f==0, image, f)
 
-    output_file.write(res)
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
 
-    cv2.imshow("magic", res)
-    cv2.waitKey(1)
-
-
-cap.release()
-output_file.release()
+video.release()
 cv2.destroyAllWindows()
